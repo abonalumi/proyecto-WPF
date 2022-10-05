04a254c6edcf6de1f1e4782a14d5b7e7e1ad5dfb
abonalumi <augustobonalumi@hotmail.com>
2022-10-05 18:24:26 -0300

    Interfaz con Windows Presentation Foundation. Aplicación de escritorio.

---
+++ WpfApp/ProyectoUno/Formulario.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoUno
+{
+    /// <summary>
+    /// Interaction logic for Formulario.xaml
+    /// </summary>
+    public partial class Formulario : Page
+    {
+        public int Id = 0;
+        public Formulario(int id=0)
+        {
+            InitializeComponent();
+            this.Id = id;
+            if (Id != 0) {
+                using (Models.WPF_AppDBEntities db = new Models.WPF_AppDBEntities())
+                {
+                    var persona = db.Persons.Find(Id);
+                    tbNombre.Text = persona.Name;
+                    tbEdad.Text = persona.Age.ToString();
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(Id == 0)
+            {
+                using (Models.WPF_AppDBEntities db = new Models.WPF_AppDBEntities())
+                {
+                    var persona = new Models.Persons();
+                    persona.Name = tbNombre.Text;
+                    persona.Age = int.Parse(tbEdad.Text);
+
+                    db.Persons.Add(persona);
+                    db.SaveChanges();
+
+                    MainWindow.StaticMainFrame.Content = new MenuYLista();
+                }
+            }
+            else
+            {
+                using (Models.WPF_AppDBEntities db = new Models.WPF_AppDBEntities())
+                {
+                    var persona = db.Persons.Find(Id);
+                    persona.Name = tbNombre.Text;
+                    persona.Age = int.Parse(tbEdad.Text);
+
+                    db.Entry(persona).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+
+                    MainWindow.StaticMainFrame.Content = new MenuYLista();
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp/ProyectoUno/MenuYLista.xaml.cs
@@ -0,0 +1,78 @@
+﻿using ProyectoUno.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoUno
+{
+    /// <summary>
+    /// Interaction logic for MenuYLista.xaml
+    /// </summary>
+    public partial class MenuYLista : Page
+    {
+        public MenuYLista()
+        {
+            InitializeComponent();
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            List<PersonViewModel> PersonList = new List<PersonViewModel>();
+            using (Models.WPF_AppDBEntities db = new Models.WPF_AppDBEntities())
+            {
+                PersonList = (
+                        from person in db.Persons
+                        select new PersonViewModel
+                        {
+                            Name = person.Name,
+                            Age = person.Age,
+                            Id = person.Id
+                        }
+                    ).ToList();
+            }
+
+            PersonDG.ItemsSource = PersonList;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.StaticMainFrame.Content = new Formulario();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int Id = (int)((Button)sender).CommandParameter;
+            using (Models.WPF_AppDBEntities db = new Models.WPF_AppDBEntities())
+            {
+                var persona = db.Persons.Find(Id);
+                db.Persons.Remove(persona);
+                db.SaveChanges();
+            }
+
+            Refresh();
+        }
+
+        private void Button_Editar_Click(object sender, RoutedEventArgs e)
+        {
+            int Id = (int)((Button)sender).CommandParameter;
+            Formulario formulario = new Formulario(Id);
+
+            MainWindow.StaticMainFrame.Content = formulario;
+        }
+
+    }
+
+
+}
